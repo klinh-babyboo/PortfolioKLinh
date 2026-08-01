d51ae1dda8994d1e52d10353834f39fdeabba195
klinh-babyboo <linhnk.cfaa@gmail.com>
2026-08-01 23:49:54 +0700

    Update header name to Nguyen Khanh Linh and brand title to KhanhLinh

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -29,7 +29,7 @@ export const Navigation: FC<NavigationProps> = ({ onOpenMobileMenu }) => {
         >
           <path d="M 256 64 L 256 128 L 192.5 128 L 160 95 L 128 64 L 96 95 L 63.5 128 L 64 128 L 128 192 L 128 256 L 64.5 256 L 32 223 L 0 192 L 0 64 L 64 0 L 192 0 Z M 256 192 L 256 256 L 192.5 256 L 160 223 L 128 192 L 128 128 L 192 128 Z" />
         </svg>
-        <span className="font-instrument-serif text-xl sm:text-2xl tracking-wide text-white">Nguyen KhanhLinh</span>
+        <span className="font-instrument-serif text-xl sm:text-2xl tracking-wide text-white">Nguyen Khanh Linh</span>
       </a>
 
       {/* Desktop center pill nav (hidden on mobile) */}
